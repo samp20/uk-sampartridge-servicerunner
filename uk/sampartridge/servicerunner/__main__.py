31d2ca7a4fb3a1d06ad6a5be87132643471f5eb7
Samuel Partridge <partridgeSL@cardiff.ac.uk>
2018-10-05 02:16:45 +0100

    Add config includes

--- uk/sampartridge/servicerunner/__main__.py
+++ uk/sampartridge/servicerunner/__main__.py
@@ -13,7 +13,8 @@ def main():
     logging.basicConfig(level=logging.DEBUG)
 
     config = configparser.ConfigParser()
-    config.read_file(open(args.config))
+    load_config(config, args.config)
+
     runner = Runner(config)
     runner.load_services()
 
@@ -28,6 +29,17 @@ def main():
         if hasattr(loop, 'shutdown_asyncgens'):
             loop.run_until_complete(loop.shutdown_asyncgens())
     loop.close()
+
+def load_config(parser, path):
+    with open(path, 'rb') as f:
+        parser.read_file(f)
+        incs = parser.get('General', 'includes', fallback=None)
+        if incs is not None:
+            del parser['General']['includes']
+            dirname = os.path.dirname(path)
+            for pth in incs.split('\n'):
+                pth = os.path.join(dirname, pth.strip())
+                load_config(parser, pth)
 
 if __name__ == '__main__':
     main()
